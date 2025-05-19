f6794b206061743f444cc1a3cde87b343f3281df
Foysal <ahmedmdfoysal123@gmail.com>
2025-05-19 07:05:12 +0600

    secured api keys to the env

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB_yYdWLJ66WquZJ26NUmwbsg49XX2ppjQ",
-  authDomain: "naturafit-2da36.firebaseapp.com",
-  projectId: "naturafit-2da36",
-  storageBucket: "naturafit-2da36.firebasestorage.app",
-  messagingSenderId: "98832324574",
-  appId: "1:98832324574:web:9b92c021dfc693285c2f4f",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
